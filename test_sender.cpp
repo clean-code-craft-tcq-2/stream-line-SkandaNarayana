392e7d284a594d918fb3bbdd8fb92648b8a7a63a
Skanda <skanda.narayana@in.bosch.com>
2022-04-06 13:28:57 +0530

    changed absolute file path

--- test_sender.cpp
+++ test_sender.cpp
@@ -7,7 +7,7 @@ TEST_CASE("test reading from a file for Temperature"){
     std::unordered_map<std::string , std::vector<int>> expected = {
             {"TEMPERATURE", {1, 4}}
     };
-    std::string filePath = "../temperatureRangeOfValues.txt";
+    std::string filePath = "temperatureRangeOfValues.txt";
     Sender senderObj;
     assert(senderObj.readFromFile(filePath, "TEMPERATURE") == expected);
     senderObj.writeToConsole();
@@ -17,7 +17,7 @@ TEST_CASE("test reading from a file for SOC"){
     std::unordered_map<std::string , std::vector<int>> expected = {
             {"SOC", {10, 20}}
     };
-    std::string filePath = "../SoCValuesFromSensor.txt";
+    std::string filePath = "SoCValuesFromSensor.txt";
     Sender senderObj;
     assert(senderObj.readFromFile(filePath, "SOC") == expected);
     senderObj.writeToConsole();
@@ -25,8 +25,8 @@ TEST_CASE("test reading from a file for SOC"){
 
 TEST_CASE("write to console"){
     std::string expected = "SOC,10 20 \nTEMPERATURE,1 4 \n";
-    std::string filePath = "../temperatureRangeOfValues.txt";
-    std::string filePath2 = "../SoCValuesFromSensor.txt";
+    std::string filePath = "temperatureRangeOfValues.txt";
+    std::string filePath2 = "SoCValuesFromSensor.txt";
     Sender senderObj;
     senderObj.readFromFile(filePath,"TEMPERATURE");
     senderObj.readFromFile(filePath2,"SOC");
